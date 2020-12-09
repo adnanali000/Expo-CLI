3608635a7ba0b124c7ab64a43fb4d3dfdc67bb76
Adnan musil <adnan.museel123@gmail.com>
2020-12-09 10:27:54 +0500

    my-first expo cli

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/favicon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={{url:"https://media.zigcdn.com/media/content/2020/Jun/2020-iron-883-motorcycle-g2.jpg"}} style={{width:300, height:230}} />
+      <Text style={{color:'#888',fontSize:20}} style={styles.Text}>
+        Hello World.
+        This is my first expo app
+      </Text>
+      <Image source={logo} style={styles.logo} /> 
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width:50,
+    height:50,
+    marginTop:10
+  },
+  Text:{
+    margin:10,
+    fontSize:20
+  }
+
 });
